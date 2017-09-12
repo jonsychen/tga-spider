f877d4b31e310099e54d1761957e09ecc8e121ab
jonsy <work@jonsy.me>
2017-09-12 13:48:54 +0800

    优化在线曲线

--- src/main/java/com/tga/TgaSampleV1.java
+++ src/main/java/com/tga/TgaSampleV1.java
@@ -20,8 +20,8 @@ public class TgaSampleV1 {
             System.out.println("check auth fail, please call manager!!!");
             return;
         }
-
-        ExecutorService executorService = Executors.newFixedThreadPool(PropertyUtil.getInt("thread.max"));
+        int threadMax = PropertyUtil.getInt("thread.max");
+        ExecutorService executorService = Executors.newFixedThreadPool(threadMax);
         String uri = "http://tga.qq.com/match/2017/pc_game.html?game=cfm";
         String videoUri = PropertyUtil.getString("video.uri");
         videoUri = videoUri.substring(0, videoUri.indexOf("time") + 5) + System.currentTimeMillis() / 1000 +
@@ -29,13 +29,19 @@ public class TgaSampleV1 {
 
         double videoTime = PropertyUtil.getDouble("video.time.length") * 6.0;
         long startTime = System.currentTimeMillis();
-        for (int i = 0; i < PropertyUtil.getInt("task.max"); i++) {
+        int taskMax = PropertyUtil.getInt("task.max");
+        int watchTime = PropertyUtil.getInt("video.time.length");
+        long sleepTime  = watchTime*60*1000/threadMax;
+        System.out.println("sleepTime: "+sleepTime);
+        for (int i = 0; i < taskMax; i++) {
             executorService.execute(new Play(uri, videoUri, videoTime, videoDownSize, httpTimeout, exceptionStatus,
                     autoIndex));
             //控制放量,缓冲坡度
-            if (i != 0 && i % 200 == 0) {
-                Thread.sleep(20 * 1000);
-            }
+//            if (i != 0 && i % 200 == 0) {
+//                Thread.sleep(20 * 1000);
+//            }
+                Thread.sleep(sleepTime);
+                
         }
 
         Thread.sleep(1000 * 60 * PropertyUtil.getInt("total.run.time"));
